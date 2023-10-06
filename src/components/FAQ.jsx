12d694f7ccfeaf86fed15d1ec5ad4f9fe4e7079c
thiagolucio <THIAGO.SYSMAP@fleetcor.com.br>
2023-10-06 12:14:19 -0300

    work in faq

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -9,24 +9,27 @@ import Vigia from "../assets/images/g1sc/g1.svg";
 const FAQData = [
   {
     question: "G1 SANTA CATARINA",
-    image: <img src={g1Sc.src} alt="Site G1 Santa Catarina" className="w-[45%] pr-6" />,
-    answer: 
-      "'Trabalhando na NSC Comunicações eu trabalhei em alguns projetos da empresa. Dentre eles o G1 de Santa Catarina onde trabalhava com o time do Rio de Janiero Remotamente no desenvolvimento da versão Web do Portal. \n O trabalho tinha como cotidiano trabalhar com GraphQL, ReactJS e NextJS'",
+    image: <img src={g1Sc.src} alt="Site G1 Santa Catarina" className="w-[45%] mr-6 rounded-xl" />,
+    answer: <div><p>Dentre os projetos que trabalhei na NSC Comunicações. Destaca-se o Portal G1 de Santa Catarina onde trabalhei como Dev Front-end junto com o time no Rio de Janeiro.</p><p className="mt-3"> O cotidiano das atividades era lidar com as tasks diárias do Jira e fazer reuniões diárias. Trabalhei com projetos React ou NextJS como Front-end e 100% remoto.</p><p className="mt-3"> Com Backend lidando com recursos NestJS, GraphQL, NodeJS, PosgreSQL de forma superficial. Importante reforçar que eu não trabalhei em projetos Mobile com React Native. Somente WebApp e PWA.</p></div>,
+    url: <a href="https://g1.globo.com/sc/santa-catarina/" target="_blank" className="inline-block text-center px-4 w-full rounded-xl rounded-t-xl custom-button-colored font-bold leading-loose mt-16"><i className="fa-solid fa-arrow-up-right-from-square mr-2"></i>Conheça o projeto</a>,
   },
   {
-    question: "How to claim your 25% discount offer?",
+    question: "NIX da NEXXERA",
     image: <img src={Nix.src} alt="Site do Nix Gateway de Pagamento" className="w-[45%] pr-6" />,
-    answer: "Our dedicated support team is here to help. You can reach out to us through the contact form on our website, send an email, or engage with us via live chat. We'll be happy to assist you with any questions or concerns you may have",        
+    answer: "Our dedicated support team is here to help. You can reach out to us through the contact form on our website, send an email, or engage with us via live chat. We'll be happy to assist you with any questions or concerns you may have",
+    url: "https://www.minhanix.com.br/",     
   },
   {
     question: "What's your refund policy?",
     image: <img src={Rizoluto.src} alt="Site Rizoluto" className="w-[45%] pr-6" />,
-    answer: "Our dedicated support team is here to help. You can reach out to us through the contact form on our website, send an email, or engage with us via live chat. We'll be happy to assist you with any questions or concerns you may have",        
+    answer: "Our dedicated support team is here to help. You can reach out to us through the contact form on our website, send an email, or engage with us via live chat. We'll be happy to assist you with any questions or concerns you may have",
+    url: "https://www.minhanix.com.br/",    
   },
   {
     question: "How to get support for the product?",
     image: <img src={Vigia.src} alt="Site Vigia da Suntech" className="w-[45%] pr-6" />,
-    answer: "Our dedicated support team is here to help. You can reach out to us through the contact form on our website, send an email, or engage with us via live chat. We'll be happy to assist you with any questions or concerns you may have",        
+    answer: "Our dedicated support team is here to help. You can reach out to us through the contact form on our website, send an email, or engage with us via live chat. We'll be happy to assist you with any questions or concerns you may have",
+    url: "https://www.minhanix.com.br/",  
   },
 ];
 
@@ -54,6 +57,7 @@ export const FAQ = () => (
                   title={item.question}
                   image={item.image}
                   content={item.answer}
+                  url={item.url}
                   key={`${item.question}-${item.answer}`}
                   defaultOpen={index === 0}
                 />
@@ -66,20 +70,20 @@ export const FAQ = () => (
   </section>
 );
 
-const FAQBox = ({ defaultOpen, title, image, content }) => {
+const FAQBox = ({ defaultOpen, title, image, content, url }) => {
   const [isOpen, setIsOpen] = useState(defaultOpen);
 
   return (
     <div
-      className="pt-2 sm:pt-6 pb-2 px-3 sm:px-8  rounded-3xl bg-customDarkBg3 custom-border-gray-darker mb-4 relative hover:bg-customDarkBg3Hover cursor-pointer"
+      className="pt-2 sm:pt-6 pb-2 px-3 sm:px-8 rounded-3xl bg-customDarkBg3 custom-border-gray-darker mb-4 relative hover:bg-customDarkBg3Hover cursor-pointer"
       onClick={() => setIsOpen(!isOpen)}
     >
-      <div className="flex flex-col p-2  justify-center items-start">
-        <h3 className=" custom-content-title pt-3 sm:pt-0 pr-8 sm:pr-0">
+      <div className="flex flex-col p-2  justify-start bg-customDarkBg2">
+        <h3 className="custom-content-title pt-3 sm:pt-0 pr-8 sm:pr-0">
           {title}
         </h3>
         <p
-          className={`text-customGrayText pt-4 transition-all duration-300 overflow-hidden ${
+          className={`text-gray-300 text-sm pt-4 transition-all duration-300 overflow-hidden ${
             isOpen ? "max-h-96" : "max-h-0"
           }`}
           >
@@ -87,6 +91,7 @@ const FAQBox = ({ defaultOpen, title, image, content }) => {
               {image}
               {content}
           </div>
+          {url}
         </p>
       </div>
       <div className="absolute top-6 right-4 sm:top-8 sm:right-8">
