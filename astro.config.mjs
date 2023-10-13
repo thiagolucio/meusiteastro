951e6e05bb4cfaa7b6e3c683a990c49d4b5b7b12
thiagolucio <THIAGO.SYSMAP@fleetcor.com.br>
2023-10-13 06:33:50 -0300

    finish site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thiagolucio.com.br',
+  compressHTML: false,
+  output: 'static',
   integrations: [react(), tailwind(), sitemap()]
 });
