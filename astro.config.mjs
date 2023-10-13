7c94badbb4699c5d53fed80741527355cb0e97ca
thiagolucio <THIAGO.SYSMAP@fleetcor.com.br>
2023-10-13 10:12:07 -0300

    trying implement translation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import astroI18next from "astro-i18next";
 
 
 // https://astro.build/config
@@ -9,5 +10,5 @@ export default defineConfig({
   site: 'https://thiagolucio.com.br',
   compressHTML: false,
   output: 'static',
-  integrations: [react(), tailwind(), sitemap()]
+  integrations: [react(), tailwind(), sitemap(), astroI18next()]
 });
