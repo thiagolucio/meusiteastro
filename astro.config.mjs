df350f55e6acdd18cdc453d4b53d47693c4cea7f
thiagolucio <THIAGO.SYSMAP@fleetcor.com.br>
2024-01-19 09:54:56 -0300

    update packages form project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thiagolucio.com.br',
-  integrations: [react(), tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()]
 });
